66913448e27aec80199048c1ec924d37b9e269e8
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-04-10 19:04:42 -0300

    profile component almost done

--- components/Pages/mainInterface/MainInterface.jsx
+++ components/Pages/mainInterface/MainInterface.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './mainInterface.css'
-import Loader from '../../loader/Loader'
+// import Loader from '../../loader/Loader'
 import Profile from '../../Profile/Profile'
 import Topbar from '../../Topbar/Topbar'
 const MainInterface = () => {
-  const [isFetching, setIsFetching] = useState(true)
-  setTimeout(() => {
-    setIsFetching(false)
-  }, 7000)
+  // const [isFetching, setIsFetching] = useState(true)
+  // setTimeout(() => {
+  // setIsFetching(false)
+  // }, 7000)
   return (
     <div className='main-interface-container'>
-      <div className={isFetching ? 'fetching' : 'interface-window'}>
-        {isFetching ? <div /> : <Topbar />}
-        {isFetching ? <Loader /> : <Profile />}
+      <div className='interface-window'>
+        <Topbar />
+        <Profile />
       </div>
     </div>
   )
--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -1,26 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './profile.css'
 const Profile = () => {
+  const [showMore, setShowMore] = useState(false)
+  const welcomeMessage = 'Welcome to my portfolio'
   return (
     <>
-      <div className='profile-container'>
-        <div className='title'>
-          <h1>About me</h1>
+      <div className='profile'>
+        <div className='profile-container'>
+          <div className='title'>
+            About me
+          </div>
+          <img className='profile-img' src='../public/images/dummy-img.png' alt='profile_img' />
+          <div className='profile-info'>
+            <p className='info-title'>Name</p>
+            <p className='info-item'>Franco Alvarez </p>
+            <p className='info-title'>Location</p>
+            <p className='info-item'>Cordoba, Argentina 📍 </p>
+            <p className='info-title'>Rol</p>
+            <p className='info-item'>Full Stack Web Developer </p>
+            <p className='info-title'>Time Coding</p>
+            <p className='info-item'>2.5 Years 📅 </p>
+          </div>
+          <div className='profile-social'>
+            <div>
+              <img className='social-img' src='../public/images/github.png' alt='github-img' />
+            </div>
+
+            <img className='social-img' src='../public/images/linkedin.png' alt='linkedin-img' />
+          </div>
         </div>
-        <img className='profile-img' src='../public/images/dummy-img.png' alt='profile_img' />
-        <div className='profile-info'>
-          <p className='info-title'>Name</p>
-          <p className='info-item'>Franco Alvarez </p>
-          <p className='info-title'>Location</p>
-          <p className='info-item'>Cordoba, Argentina 📍 </p>
-          <p className='info-title'>Status</p>
-          <p className='info-item'>Unemployed 😢 </p>
-          <p className='info-title'>Time Coding</p>
-          <p className='info-item'>2 Years 📅 </p>
-        </div>
-        <div className='profile-social'>
-          <img className='social-img' src='../public/images/github.png' alt='github-img' />
-          <img className='social-img' src='../public/images/linkedin.png' alt='linkedin-img' />
+        <div className='description-section'>
+          <div className='head-title'> {welcomeMessage}</div>
+          <div className='description'>
+            <p> I am a highly motivated, self-taught developer seeking to launch a career building web applications and services.
+              <br />I've been building and deploying web applications and web services for over 2 years.<br />
+              My next goal is to find my first job and keep growing as a developer.<br /> However, this time along side a group of experienced developers.
+            </p>
+            <button className='show-more-btn' onClick={() => setShowMore(!showMore)}> {showMore ? 'Hide' : 'Show'}  Studies</button>
+          </div>
+
+          {showMore
+            ? <>
+              <div className='description-more'>
+                <p className='ed-title'>Egg Coorporation Bootcamp <span style={{ fontSize: '16px', fontWeight: '500', marginLeft: '360px' }}>Degree: </span><span style={{ fontSize: '14px', fontWeight: '400', color: '#3E9F3D' }}>Full Stack Developer</span></p>
+                <span className='date-title'>Duration: from Jul/2021 to Jul/2022</span>
+                <p style={{ marginTop: '10px' }}> By the end of this bootcamp, I was able to build my own website with Java (Spring-boot Framework)<br /> and MySQL as backend, and also learned the basics of HTML, CSS and Bootstrap as frontend.<br />
+                </p>
+              </div>
+              <div className='description-more-2'>
+                <p className='ed-title'>Self-Taugth</p>
+                <span className='date-title'>Duration: July/2022 till now</span>
+                <p style={{ marginTop: '10px' }}> As soon as the previous bootcamp ends, I started to learn on my own with online material.<br />
+                  I've switched from Java to Node.js and started to learn React.js along side good practices such as reusable components,<br /> Hooks, Virtual DOM, state/props & much more !
+                </p>
+              </div>
+            </>
+
+            : <div />}
         </div>
       </div>
     </>
