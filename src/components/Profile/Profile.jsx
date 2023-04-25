b021913c6bdde1a8c4de74a881857947650f53c0
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-04-25 16:41:36 -0300

    Description component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-closing-tag-location */
 import { Title, SectionTitle } from '../ReusableComponents/Title/Title'
+import { Description, Studies } from '../ReusableComponents/Description/Description'
 import Infoitem from '../ReusableComponents/InfoItem/Infoitem'
-import React, { useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import FileDownloadIcon from '@mui/icons-material/FileDownload'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import ConstructionIcon from '@mui/icons-material/Construction'
@@ -11,11 +12,27 @@ import Curriculum from '../../../public/Curriculum.pdf'
 import DoneIcon from '@mui/icons-material/Done'
 import Resume from '../../../public/Resume.pdf'
 import './profile.css'
+
 const Profile = ({ english }) => {
   const [showMore, setShowMore] = useState(false)
-  setTimeout(() => {
-    setShowMore(true)
-  }, 2500)
+  useEffect(() => {
+    setTimeout(() => {
+      setShowMore(true)
+    }, 2500)
+  }, [])
+  useEffect(() => {
+    setShowMore(false)
+    setTimeout(() => {
+      setShowMore(true)
+    }, 2500)
+  }, [english])
+
+  const description = ['I am a highly motivated, self-taught developer seeking to launch a career building web applications and services.', 'I\'ve been building and deploying web applications and web services for over 2 years.', 'My next goal is to find my first job and keep growing as a developer.However, this time along side a group of experienced developers']
+  const descripcion = ['Soy un desarrollador altamente motivado y autodidacta que busca iniciar una carrera construyendo aplicaciones y servicios web.', 'He estado construyendo y desplegando aplicaciones y servicios web durante más de 2 años.', 'Mi próximo objetivo es encontrar mi primer trabajo y seguir creciendo como desarrollador, <br /> pero esta vez junto a un grupo de desarrolladores experimentados']
+  const studio1 = ['Al final de este bootcamp, fui capaz de construir mi propio sitio web con Java (Spring-boot como framework),', 'y MySQL como backend, también aprendí los conceptos básicos de HTML, CSS y Bootstrap para el Frontend.']
+  const study1 = ['By the end of this bootcamp, I was able to build my own website with Java (Spring-boot Framework),', 'and MySQL as backend, and also learned the basics of HTML, CSS and Bootstrap as frontend.']
+  const studio2 = ['Tan pronto como finalizó el bootcamp, comencé a aprender por mi cuenta con material en online.', 'Cambié de Java a Node.js y comencé a aprender React.js junto a buenas prácticas como componentes reutilizables,', 'Hooks, Virtual DOM, State/props y más.']
+  const study2 = ['As soon as the previous bootcamp ends, I started to learn on my own with online material.', "I've switched from Java to Node.js and started to learn React.js along side good practices such as reusable components,", 'Hooks, Virtual DOM, state/props & much more !']
   const welcomeMessage = 'Welcome to my portfolio'
   const mensajeBienvenida = 'Bienvenido a mi portfolio!'
   return (
@@ -47,58 +64,26 @@ const Profile = ({ english }) => {
         </div>
         <div className='description-section'>
           <Title styles='head-profile-title' text={english ? welcomeMessage : mensajeBienvenida} />
-          <div className='description'>
-            {english
-              ? <p> I am a highly motivated, self-taught developer seeking to launch a career building web applications and services.
-                <br />I've been building and deploying web applications and web services for over 2 years.<br />
-                My next goal is to find my first job and keep growing as a developer.<br /> However, this time along side a group of experienced developers.
-              </p>
-              : <p>Soy un desarrollador altamente motivado y autodidacta que busca iniciar una carrera construyendo aplicaciones y servicios web.
-                <br />He estado construyendo y desplegando aplicaciones y servicios web durante más de 2 años.<br />
-                Mi próximo objetivo es encontrar mi primer trabajo y seguir creciendo como desarrollador, <br /> pero esta vez junto a un grupo de desarrolladores experimentados.
-              </p>}
-
-            {english
-              ? <button className='show-more-btn' onClick={() => setShowMore(!showMore)}> {showMore ? 'Hide' : 'Show'}  Studies</button>
-              : <button className='show-more-btn' onClick={() => setShowMore(!showMore)}> {showMore ? 'Esconder' : 'Mostrar'} Estudios</button>}
-
-          </div>
-
+          <Description english={english} setShowMore={setShowMore} showMore={showMore} text={english ? description : descripcion} styles='description' btnStyles='show-more-btn' />
           {showMore
-            ? <> {english === true
-              ? <>
-                <div className='description-more'>
-                  <p className='ed-title'>Egg Coorporation Bootcamp <span style={{ fontSize: '15px', fontWeight: '500', marginLeft: '325px' }}>Degree: </span><span style={{ marginLeft: '5px', fontSize: '14px', fontWeight: '600', color: '#3E9F3D' }}>Full Stack Web Developer</span></p>
-                  <span className='date-title'>Duration: from Jul/2021 to Jul/2022</span>
-                  <p style={{ marginTop: '10px' }}> By the end of this bootcamp, I was able to build my own website with Java (Spring-boot Framework)<br /> and MySQL as backend, and also learned the basics of HTML, CSS and Bootstrap as frontend.<br />
-                  </p>
-                </div><div className='description-more-2'>
-                  <p className='ed-title'>Self-Taugth</p>
-                  <span className='date-title'>Duration: July/2022 till now</span>
-                  <p style={{ marginTop: '10px' }}> As soon as the previous bootcamp ends, I started to learn on my own with online material.<br />
-                    I've switched from Java to Node.js and started to learn React.js along side good practices such as reusable components,<br /> Hooks, Virtual DOM, state/props & much more !
-                  </p>
-                </div>
-              </>
-              : <>
-                <div className='description-more'>
-                  <p className='ed-title'>Egg Coorporation Bootcamp <span style={{ fontSize: '15px', fontWeight: '500', marginLeft: '325px' }}>Titulo: </span><span style={{ marginLeft: '5px', fontSize: '14px', fontWeight: '600', color: '#3E9F3D' }}>Full Stack Web Developer</span></p>
-                  <span className='date-title'>Duracion: Desde Jul/2021 Hasta Jul/2022</span>
-                  <p style={{ marginTop: '10px' }}> Al final de este bootcamp, fui capaz de construir mi propio
-                    sitio web con Java (Spring-boot como framework),<br /> y MySQL como backend, también
-                    aprendí los conceptos básicos de HTML, CSS y Bootstrap para el Frontend.
-                  </p>
-                </div><div className='description-more-2'>
-                  <p className='ed-title'>Auto-Didacta</p>
-                  <span className='date-title'>Duracion: Desde Jul/2022 hasta la fecha</span>
-                  <p style={{ marginTop: '10px' }}>
-                    Tan pronto como finalizó el bootcamp, comencé a aprender por mi cuenta
-                    con material en online.<br /> Cambié de Java a Node.js y comencé a aprender
-                    React.js junto a buenas prácticas como componentes reutilizables,<br /> Hooks,
-                    Virtual DOM, State/props y más.
-                  </p>
-                </div>
-              </>}
+            ? <>
+              <Studies
+                text={english ? study1 : studio1}
+                date={english ? 'Duration: from Jul/2021 to Jul/2022' : 'Duracion: Desde Jul/2021 Hasta Jul/2022'}
+                title='Egg Coorporation Bootcamp'
+                titleStyles='company-title'
+                dateStyles='date-title'
+                styles='studies'
+                degree={<><span style={{ fontSize: '15px', fontWeight: '500', marginLeft: '325px' }}>Degree: </span><span style={{ marginLeft: '5px', fontSize: '14px', fontWeight: '600', color: '#3E9F3D' }}>Full Stack Web Developer</span></>}
+              />
+              <Studies
+                text={english ? study2 : studio2}
+                date={english ? 'Duration: July/2022 till now' : 'Duracion: Desde Jul/2022 hasta la fecha'}
+                title={english ? 'Self-Taught' : 'Auto-Didacta'}
+                titleStyles='company-title'
+                dateStyles='date-title'
+                styles='studies'
+              />
             </>
             : <div />}
         </div>
---
+++ src/components/ReusableComponents/Description/Description.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-lone-blocks */
+import React from 'react'
+import './description.css'
+
+const Description = ({ showMore, setShowMore, text, english, styles, btnStyles }) => {
+  return (
+    <div key={text} className={styles}>
+      {text.map((m, idx) => (
+        <p key={idx} className='desc-styles'>{m}</p>
+      ))}
+
+      {english
+        ? <button className={btnStyles} onClick={() => setShowMore(!showMore)}> {showMore ? 'Hide' : 'Show'}  Studies</button>
+        : <button className={btnStyles} onClick={() => setShowMore(!showMore)}> {showMore ? 'Esconder' : 'Mostrar'} Estudios</button>}
+
+    </div>
+  )
+}
+
+const Studies = ({ date, title, text, styles, titleStyles, dateStyles, degree }) => {
+  return (
+    <div key={text} className={styles}>
+      <p className={titleStyles}>{title} {degree}</p>
+      <span className={dateStyles}>{date}</span>
+      {text.map((m, idx) => (
+        <p key={idx} style={{ marginTop: '3px' }}>
+          {m}
+        </p>
+      ))}
+
+    </div>
+  )
+}
+
+export {
+  Description,
+  Studies
+}
